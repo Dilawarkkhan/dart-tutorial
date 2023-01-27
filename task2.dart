5f0f76133662b12af0941132d7479ffda1eacde8
ahmedawwan <ahmedsaeed0973@gmail.com>
2023-01-27 10:09:51 +0500

    grade task added

---
+++ task2.dart
@@ -0,0 +1,13 @@
+void main(){
+  //TODO: Create a program to calculate grade of the student
+
+  //TODO: Take input from user in integers
+
+  print(getGrade(100));
+}
+
+
+//TODO: Complete the function
+String getGrade(int marks){
+  return '';
+}
