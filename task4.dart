5466538c0f1f00824b724ff5e0149a1f01cd95dd
Dilu3135 <dilawarkhan2015@hotmail.com>
2023-01-31 09:20:52 +0500

    task

--- task4.dart
+++ task4.dart
@@ -1,6 +1,6 @@
 // Operator: (+ - * /): 
-// take input from user (if user enters stop then stop the programs) hint: loop
-// if user enters something beside the given inputs print('invalid input, please try again'); and restart the program
-// take two values from user
-// print result
+// take input from user (if user enters stop then stop the programs) hint: loop.
+// if user enters something beside the given inputs print('invalid input, please try again'); and restart the program.
+// take two values from user.
+// print result.
 // loops, switch statements will be used.
--- task5.dart
+++ task5.dart
@@ -6,11 +6,16 @@ void main() {
   print('How many subjects do you have?');
   int userSubj = int.parse(stdin.readLineSync()!);
 
-
   for (int i = 1; i <= userSubj; i++) {
     double userInput = double.parse(stdin.readLineSync()!);
-    totalSum = userInput + totalSum;
+    totalSum += userInput;
+    // totalSum = totalSum + userInput;
   }
-
   print(totalSum / userSubj);
 }
+
+// void test() {
+//   int j = 0;
+//   j = j + 2;
+//   j += 1;
+// }
